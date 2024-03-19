23f7f3aac7c52e8d21228873da28c6a3cfd35977
byemidev <emilio.arevalozuniga@gmail.com>
2024-03-19 20:40:03 +0100

    updating repo about class exercise

--- semana3_dia1_compras%forms%PHP/back-end/show_articulos.php
+++ semana3_dia1_compras%forms%PHP/back-end/show_articulos.php
@@ -1,16 +1,15 @@
 <?php
-    
-    require_once('getConn.php')
+    echo"ejecutandose articulos"
+/*
+    require_once('gestorDB.php');
     $conn = getConn();
     
-    $nombre = $_POST["nombre_art"];
-    $cod = $_POST["codigo_art"];
-    $des = $_POST["descripcion_art"];
-    $price = $_POST["precio"];
+    $sql = "SELECT * FROM ARTICULOS";
 
-    $sql = ;//todo 
-
+    require_once('gestorDB.php');
+    show($conn, $sql);
     //TODO: make if and nested while to iterate in the array
-
     $conn->close();
+
+*/
 ?>
