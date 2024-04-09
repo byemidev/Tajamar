d17d1f4bcbf026141ac94535444bb0ebcb9fb18d
byemidev <emilio.arevalozuniga@gmail.com>
2024-04-09 21:02:34 +0200

    adding week 6 day 2 , a lot of task to have to do

---
+++ semana6_dia2_Basics_HTML&CSS&JS_hotel/script.js
@@ -0,0 +1,33 @@
+
+//Mostrar form para cliente nuevo cuando btnNewClient is cliked
+const btnNewClient = document.getElementById("btnNewClient");
+const formNewClient = document.getElementById("form__display__newclient");
+
+btnNewClient.addEventListener("click", () => {
+  if (formNewClient.style.visibility === "hidden") {
+    formNewClient.style.visibility = "visible";
+  } else {
+    formNewClient.style.visibility = "hidden";
+  }
+});
+
+//clase para instanciar objetos de mis clientes
+class cliente {
+   username;
+    constructor(username){
+        this.username = username;
+    }
+}
+
+//formNewCLient y mostrar el login
+const btnClient = document.getElementById("btnClient");
+btnClient.addEventListener("click" , () => {
+    formNewClient.style.visibility = "hidden";
+    location.href = "./paginas/reserva.html";
+});
+
+formNewClient.addEventListener("submit",(event) => {
+    event.preventDefault();
+});
+
+
