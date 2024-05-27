a57252cd38a82b9c3927cc63c6b69df120660435
byemidev <emilio.arevalozuniga@gmail.com>
2024-05-27 21:14:23 +0200

    adding new empleadosCRUD project

---
+++ csharp/empleadosCRUD/empleadosCRUD/Model/departamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empleadosCRUD.Model
+{
+    internal class departamento
+    {
+        private int codigo;
+        private string nombre;
+        private double presupuesto, gastos;
+
+        public departamento(int codigo, string nombre, double presupuesto, double gastos) {
+            this.codigo = codigo;
+            this.nombre = nombre;   
+            this.presupuesto = presupuesto;
+            this.gastos = gastos;
+        }
+
+        public override string ToString()
+        {
+            return ""; 
+        }
+    }
+}
---
+++ csharp/empleadosCRUD/empleadosCRUD/Model/empleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empleadosCRUD.Model
+{
+    internal class empleado
+    {
+        private int codigo, codigo_departamento;
+        private string  nif, nombre, apellido1, apellido2;
+
+        public empleado(int codigo, string nif, string nombre, string apellido1, string apellido2, int codigo_departamento) { 
+            this.codigo = codigo;
+            this.nif = nif;
+            this.nombre = nombre;
+            this.apellido1 = apellido1;
+            this.apellido2 = apellido2;
+            this.codigo_departamento = codigo_departamento; 
+        }
+
+        public  override string ToString() {
+            return "codigo: " + this.codigo + "nif: " + this.nif + "nombre: " + this.nombre + "apellido: " + this.apellido1 + "apellido: " + this.apellido2 + "codigo dep" + this.codigo_departamento;
+        }
+    }
+}
