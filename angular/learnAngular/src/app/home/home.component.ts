263621e5579f4da26a1730464771cf55c5a7ddc7
byemidev <byemidev@gmail.com>
2024-09-13 19:46:05 +0200

    upgrading angular program , using http (Api rest), using json-serve package for deploy json as a server , logic and refactoring

--- angular/learnAngular/src/app/home/home.component.ts
+++ angular/learnAngular/src/app/home/home.component.ts
@@ -15,15 +15,13 @@ import { HousingService } from '../housing.service';
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="primary"  type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
-  <app-housing-location
-  *ngFor="let housingLocation of housingLocationList"
-  [housingLocation]="housingLocation">
-</app-housing-location>
+  <!-- ngFor suports to static html to iterations or loops in order to access into data collections -->
+  <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location> 
   </section>
   `,
   styleUrl: './home.component.css'
@@ -33,8 +31,30 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: Housinglocation[] = [];
   housingService: HousingService = inject(HousingService);
+  
+  filteredLocationList: Housinglocation[] = [];
+  
+  filterResults(text: string){ //in our class in order to using into our component we need to building here and theirs instances for objects or data types 
+    
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    
+    this.filteredLocationList= this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList: Housinglocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 }
+
+//lesson 14 for adding http communication
+// npm install -g json-server
+//
